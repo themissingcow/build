8f3677d84c5b80f654da43620881cf5d395bce9a
Tom Cowland <tomc@cinesite.com>
2019-05-28 14:43:23 +0100

    Build : Allow the Docker image to be built with build.py
    The 'build' project can now be specified, which will build the Docker
    image from the local checkout, and optionally push this to Dockerhub
    (if `--upload 1` is set).
    
    Builds of the other project now use the published images (or their local
    equivalents) instead of directly building the image each time.
    
    Important changes :
    
     - `--docker` is now `--use-docker` to avoid ambiguity when building the
       docker image.
    
     - `--build-env` overrides the dockerhub image tag to use for builds of
       the 'gaffer' and 'dependencies' projects.
    
     - `--tag-as` defines the tag used to tag the image in image builds.
    
     - `--no-cache` Can be set to disable docker's layer cache during image
       builds (see README.md)
    
     - `--update-version-locks` and --new-only` Allow the yum package versions
       to be updated during an image build.

--- README.md
+++ README.md
@@ -43,3 +43,55 @@ Remove stopped containers :
 Remove old images :
 
 `docker images -q --filter dangling=true | xargs docker rmi`
+
+Building the build environment
+------------------------------
+
+Gaffer and Dependency builds are made using pre-published docker images. These
+environments are built from the Dockerfile in this repository. The `build.py`
+script also aids the build/publish of these images. For example:
+
+ `./build.py --project build --tag-as 1.1.0 --upload 1`
+
+In order to produce this image, we make heavy use of `yum` to install the
+packages required to build Gaffer and it's dependencies. As we don't manually
+specify every version of every package we risk a non-deterministic build
+environment. To get around this, we make use of `yum versionlock`. The
+`yum-versionlock.list` file in the repository is copied into the base centos
+image such that when `yum` runs, it will repeatable install the expected
+versions. In order to help manage this, the `build.py` script has a few options
+to aid updating of the lock list when new packages are added of updated required.
+
+ - `--update-version-locks [0|1]` When set (defaults to `0`), this will ignore
+   all version locks and update `yum-versionlock.list` to the 'current' version
+   of all packages installed during docker's build. The revised file can then
+   be commit, tagged and a new docker image pushed to docker hub.
+
+ - `--new-only [0|1]` When set (defaults to `0`) the existing version lock list
+   will not be cleared. This allows the versions to be locked for any new
+   packages installed by changes to the `Dockerfile` without affecting the
+   versions of existing packages.
+
+ - `--upload [0|1]` Will push the built image to a docker hub tag.
+
+### Cheat sheet
+
+Added a new package to the image, installed with yum in the Dockerfile:
+
+`./build.py --project build --update-version-locks 1 --new-only 1 --tag-as x.x.x`
+
+Update all packages to latest:
+
+`./build.py --project build --update-version-locks 1 --tag-as x.x.x`
+
+### A note on Docker's caching mechanism
+
+Docker caches layers based on the `RUN` command string. As such, it does not
+known when the version lock file changes. the `--update-version-locks` command
+will always run with `--no-cache` set, but if you've just pulled some updates
+from upstream, and are re-building on your machine, you may find docker will
+be using an out-of-date cache of one or more of the layers.
+
+As such, its recommended to use `--no-cache 1` whenever performing release
+builds.
+
--- build.py
+++ build.py
@@ -48,6 +48,13 @@ import multiprocessing
 parser = argparse.ArgumentParser()
 
 parser.add_argument(
+	"--build-env",
+	dest = "buildEnv",
+	default = "1.0.0",
+	help = "The container image tag to use for docker builds."
+)
+
+parser.add_argument(
 	"--organisation",
 	default = "GafferHQ",
 	help = "The GitHub organisation containing the project to build."
@@ -56,7 +63,7 @@ parser.add_argument(
 parser.add_argument(
 	"--project",
 	default = "gaffer",
-	choices = [ "gaffer", "dependencies" ],
+	choices = [ "gaffer", "dependencies", "build" ],
 	help = "The project to build."
 )
 
@@ -80,6 +87,7 @@ parser.add_argument(
 
 parser.add_argument(
 	"--version",
+	default = "",
 	help = "The version to build. Can either be a tag or SHA1 commit hash."
 )
 
@@ -92,11 +100,49 @@ parser.add_argument(
 )
 
 parser.add_argument(
-	"--docker",
+	"--use-docker",
+	dest = "docker",
 	type = distutils.util.strtobool,
 	default = "linux" in sys.platform,
 	help = "Performs the build using a Docker container. This provides a "
 	       "known build platform so that builds are repeatable."
+)
+
+parser.add_argument(
+	"--tag-as",
+	type = str,
+	dest = "tagAs",
+	default = "latest",
+	help = "The tag to apply to the build docker image when building the 'build' project."
+)
+
+parser.add_argument(
+	"--no-cache",
+	dest = "dockerNoCache",
+	type = distutils.util.strtobool,
+	default = False,
+	help = "When building the 'build' project, because docker caches layers "
+	       "based on the RUN command string it will fail to include any changes "
+	       "to package version locks, etc... Use this flag to ensure a clean build."
+)
+
+parser.add_argument(
+	"--update-version-locks",
+	dest = "updateVersions",
+	type = distutils.util.strtobool,
+	default = False,
+	help = "If set, will remove the package manager version locks prior to "
+	       "building the docker image to allow updated packages to be retrieved."
+)
+
+parser.add_argument(
+	"--new-only",
+	dest = "newPackagesOnly",
+	type = distutils.util.strtobool,
+	default = False,
+	help = "If set along with --update-version-locks, only newly added packages "
+	       "without an existing version lock will be updated in the lock list, "
+	       "existing packages will keep their version."
 )
 
 parser.add_argument(
@@ -109,39 +155,29 @@ parser.add_argument(
 
 args = parser.parse_args()
 
-if args.interactive :
-	if not args.docker :
-		parser.exit( 1, "--interactive requires --docker\n" )
-	if args.version or args.upload :
-		parser.exit( 1, "--interactive can not be used with other flags\n" )
+if args.project == "build" :
+
+	if args.docker :
+		parser.exit( 1, "--use-docker can't be used with the 'build' project" )
+	if args.version :
+		parser.exit( 1, "--version can't be used with the 'build' project" )
+
 else :
-	if not args.version :
-		parser.exit( "--version argument is required")
 
-# Check that our environment contains everything we need to do a build.
-
-if args.upload :
-	if "GITHUB_RELEASE_TOKEN" not in os.environ	:
-		parser.exit( 1,  "GITUHB_RELEASE_TOKEN environment variable not set" )
-
-# Check that the paths to the renderers are sane.
-
-platform = "linux" if "linux" in sys.platform or args.docker else "osx"
-libExtension = ".so" if platform == "linux" else ".dylib"
-
-if args.arnoldRoot :
-	arnoldLib = args.arnoldRoot + "/bin/libai" + libExtension
-	if not os.path.exists( arnoldLib ) :
-		parser.exit( 1, "{0} not found\n".format( arnoldLib ) )
-
-if args.delightRoot :
-	delightLib = args.delightRoot + "/lib/lib3delight" + libExtension
-	if not os.path.exists( delightLib ) :
-		parser.exit( 1, "{0} not found\n".format( delightLib ) )
+	if args.interactive :
+		if not args.docker :
+			parser.exit( 1, "--interactive requires --use-docker\n" )
+		if args.version or args.upload :
+			parser.exit( 1, "--interactive can not be used with other flags\n" )
+	else :
+		if not args.version :
+			parser.exit( "--version argument is required")
 
 # Build a little dictionary of variables we'll need over and over again
 # in string formatting operations, and use it to figure out what
 # package we will eventually be generating.
+
+platform = "linux" if "linux" in sys.platform or args.docker else "osx"
 
 formatVariables = {
 	"organisation" : args.organisation,
@@ -152,162 +188,230 @@ formatVariables = {
 	"arnoldRoot" : args.arnoldRoot,
 	"delight" : args.delightRoot,
 	"releaseToken" : "",
-	"auth" : "",
+	"auth" : ""
 }
+
 
 githubToken = os.environ.get( "GITHUB_RELEASE_TOKEN", "" )
 if githubToken :
-	formatVariables[ "releaseToken" ] = githubToken
-	formatVariables[ "auth" ] = '-H "Authorization: token %s"' % githubToken
+	formatVariables["releaseToken"] = githubToken
+	formatVariables["auth"] = '-H "Authorization: token %s"' % githubToken
 
-if args.project == "gaffer" :
-	formatVariables["uploadFile"] = "{project}-{version}-{platform}.tar.gz".format( **formatVariables )
+def dockerContainerName() :
+	return "gafferhq-build-{id}".format( id = uuid.uuid1() )
+
+if args.project == "build" :
+
+	# The build project is simpler as its always local and uses local src
+
+	imageTag = "gafferhq/build:%s" % args.tagAs
+
+	yumVersionlockPath = "yum-versionlock.list"
+	# As we can't have conditionals in the Dockerfile, we always copy in our
+	# lock file, so to 'unlock' we simply empty it out so it has no effect.
+	if args.updateVersions and not args.newPackagesOnly :
+		sys.stderr.write( "Unlocking all package versions...\n" )
+		os.remove( yumVersionlockPath )
+		open( yumVersionlockPath, 'w' ).close()
+
+	# Build the image
+	buildCommand = "docker build {cache} -t {tag} .".format(
+		cache = "--no-cache" if ( args.dockerNoCache or args.updateVersions ) else "",
+		tag = imageTag
+	)
+	sys.stderr.write( buildCommand + "\n" )
+	subprocess.check_call( buildCommand, shell = True )
+
+	if args.updateVersions :
+		# Extract the updated version lock files from the container
+		extractCommand = " && ".join((
+			"docker create --name {name} {tag}",
+			"docker cp {name}:{versionlockSrc} {versionlockDest}",
+			"docker rm {name}"
+		)).format(
+			name = dockerContainerName(),
+			tag = imageTag,
+			versionlockSrc = "/etc/yum/pluginconf.d/versionlock.list",
+			versionlockDest = yumVersionlockPath
+		)
+		sys.stderr.write( extractCommand + "\n" )
+		subprocess.check_call( extractCommand, shell = True )
+
+	if args.upload :
+		pushCommand = "docker push %s" % imageTag
+		sys.stderr.write( pushCommand + "\n" )
+		subprocess.check_call( pushCommand, shell = True )
+
 else :
-	formatVariables["uploadFile"] = "gafferDependencies-{version}-{platform}.tar.gz".format( **formatVariables )
 
-# If we're going to be doing an upload, then check that the release exists. Better
-# to find out now than at the end of a lengthy build.
+	# Check that our environment contains everything we need to do a build.
 
-def releaseId() :
+	if args.upload and "GITHUB_RELEASE_TOKEN" not in os.environ :
+		parser.exit( 1, "GITHUB_RELEASE_TOKEN environment variable not set" )
 
-	release = subprocess.check_output(
-		"curl -s {auth} https://api.github.com/repos/{organisation}/{project}/releases/tags/{version}".format(
-			**formatVariables
-		),
-		shell = True
-	)
-	release = json.loads( release )
-	return release.get( "id" )
+	# Check that the paths to the renderers are sane.
 
-if args.upload and releaseId() is None :
-	parser.exit( 1, "Release {version} not found\n".format( **formatVariables ) )
+	libExtension = ".so" if platform == "linux" else ".dylib"
 
-# Restart ourselves inside a Docker container so that we use a repeatable
-# build environment.
-
-if args.docker and not os.path.exists( "/.dockerenv" ) :
-
-	imageCommand = "docker build -t gafferhq-build .".format( **formatVariables )
-	sys.stderr.write( imageCommand + "\n" )
-	subprocess.check_call( imageCommand, shell = True )
-
-	containerEnv = []
-	if githubToken :
-		containerEnv.append( "GITHUB_RELEASE_TOKEN=%s" % githubToken )
-
-	containerMounts = []
 	if args.arnoldRoot :
-		containerMounts.append( "-v %s:/arnold:ro,Z" % args.arnoldRoot )
-		containerEnv.append( "ARNOLD_ROOT=/arnold" )
+		arnoldLib = args.arnoldRoot + "/bin/libai" + libExtension
+		if not os.path.exists( arnoldLib ) :
+			parser.exit( 1, "{0} not found\n".format( arnoldLib ) )
+
 	if args.delightRoot :
-		containerMounts.append( " -v %s:/delight:ro,Z" % args.delightRoot )
-		containerEnv.append( "DELIGHT=/delight" )
+		delightLib = args.delightRoot + "/lib/lib3delight" + libExtension
+		if not os.path.exists( delightLib ) :
+			parser.exit( 1, "{0} not found\n".format( delightLib ) )
 
-	containerName = "gafferhq-build-{id}".format( id = uuid.uuid1() )
+	# Define our upload file name
 
-	containerEnv = " ".join( containerEnv )
-	containerMounts = " ".join( containerMounts )
+	if args.project == "gaffer" :
+		formatVariables["uploadFile"] = "{project}-{version}-{platform}.tar.gz".format( **formatVariables )
+	elif args.project == "dependencies" :
+		formatVariables["uploadFile"] = "gafferDependencies-{version}-{platform}.tar.gz".format( **formatVariables )
 
-	if args.interactive :
-		containerCommand = "env {env} bash".format( env = containerEnv )
+	# If we're going to be doing an upload, then check that the release exists. Better
+	# to find out now than at the end of a lengthy build.
+
+	def releaseId() :
+
+		release = subprocess.check_output(
+			"curl -s {auth} https://api.github.com/repos/{organisation}/{project}/releases/tags/{version}".format(
+				**formatVariables
+			),
+			shell = True
+		)
+		release = json.loads( release )
+		return release.get( "id" )
+
+	if args.upload and releaseId() is None :
+		parser.exit( 1, "Release {version} not found\n".format( **formatVariables ) )
+
+	# Restart ourselves inside a Docker container so that we use a repeatable
+	# build environment.
+	if args.docker and not os.path.exists( "/.dockerenv" ) :
+
+		image = "gafferhq/build:%s" % args.buildEnv
+
+		containerEnv = []
+		if githubToken :
+			containerEnv.append( "GITHUB_RELEASE_TOKEN=%s" % githubToken )
+
+		containerMounts = []
+		if args.arnoldRoot :
+			containerMounts.append( "-v %s:/arnold:ro,Z" % args.arnoldRoot )
+			containerEnv.append( "ARNOLD_ROOT=/arnold" )
+		if args.delightRoot :
+			containerMounts.append( " -v %s:/delight:ro,Z" % args.delightRoot )
+			containerEnv.append( "DELIGHT=/delight" )
+
+		containerEnv = " ".join( containerEnv )
+		containerMounts = " ".join( containerMounts )
+		containerName = dockerContainerName()
+
+		if args.interactive :
+			containerCommand = "env {env} bash".format( env = containerEnv )
+		else :
+			containerCommand = "env {env} bash -c './build.py --project {project} --version {version} --upload {upload}'".format( env = containerEnv, **formatVariables )
+
+		dockerCommand = "docker run {mounts} --name {name} -i -t {image} {command}".format(
+			mounts = containerMounts,
+			name = containerName,
+			image = image,
+			command = containerCommand
+		)
+		sys.stderr.write( dockerCommand + "\n" )
+		subprocess.check_call( dockerCommand, shell = True )
+
+		if not args.interactive :
+			# Copy out the generated package.
+			copyCommand = "docker cp {container}:/{project}-{version}-source/{uploadFile} ./".format(
+				container = containerName,
+				**formatVariables
+			)
+			sys.stderr.write( copyCommand + "\n" )
+			subprocess.check_call( copyCommand, shell = True )
+
+		sys.exit( 0 )
+
+	# Here we're actually doing the build, this will run either locally or inside
+	# the container bootstrapped above
+
+	if os.path.exists( "/.dockerenv" ) and args.project == "gaffer" :
+
+		# Start an X server so we can generate screenshots when the
+		# documentation builds.
+		os.system( "Xvfb :99 -screen 0 1280x1024x24 &" )
+		os.environ["DISPLAY"] = ":99"
+		os.system( "metacity&" )
+
+	# Download source code
+
+	sourceURL = "https://github.com/{organisation}/{project}/archive/{version}.tar.gz".format( **formatVariables )
+	sys.stderr.write( "Downloading source \"%s\"\n" % sourceURL )
+
+	sourceDirName = "{project}-{version}-source".format( **formatVariables )
+	tarFileName = "{0}.tar.gz".format( sourceDirName )
+	downloadCommand = "curl -L {0} > {1}".format( sourceURL, tarFileName )
+	sys.stderr.write( downloadCommand + "\n" )
+	subprocess.check_call( downloadCommand, shell = True )
+
+	sys.stderr.write( "Decompressing source to \"%s\"\n" % sourceDirName )
+
+	shutil.rmtree( sourceDirName, ignore_errors = True )
+	os.makedirs( sourceDirName )
+	subprocess.check_call( "tar xf %s -C %s --strip-components=1" % ( tarFileName, sourceDirName ), shell = True )
+	os.chdir( sourceDirName )
+
+	# Download precompiled dependencies. We do this using the
+	# same script that is used to download the dependencies for
+	# testing on Travis, so that release builds are always made
+	# against the same dependencies we have tested against.
+
+	if args.project == "gaffer" :
+		subprocess.check_call( "./config/travis/installDependencies.sh", shell = True )
+
+	# Perform the build.
+
+	if args.project == "gaffer" :
+
+		# We run SCons indirectly via `python` so that it uses our
+		# preferred python from the environment. SCons itself
+		# unfortunately hardcodes `/usr/bin/python`, which might not
+		# have the modules we need to build the docs.
+		buildCommand = "python `which scons` package PACKAGE_FILE={uploadFile} ENV_VARS_TO_IMPORT=PATH DELIGHT_ROOT={delight} ARNOLD_ROOT={arnoldRoot} OPTIONS='' -j {cpus}".format(
+			cpus=multiprocessing.cpu_count(), **formatVariables
+		)
+
 	else :
-		containerCommand = "env {env} bash -c './build.py --project {project} --version {version} --upload {upload}'".format( env = containerEnv, **formatVariables )
 
-	dockerCommand = "docker run {mounts} --name {name} -i -t gafferhq-build {command}".format(
-		name = containerName,
-		mounts = containerMounts,
-		command = containerCommand
-	)
-
-	sys.stderr.write( dockerCommand + "\n" )
-	subprocess.check_call( dockerCommand, shell = True )
-
-	if not args.interactive :
-		# Copy out the generated package.
-		copyCommand = "docker cp {container}:/{project}-{version}-source/{uploadFile} ./".format(
-			container = containerName,
+		buildCommand = "env RMAN_ROOT={delight} ARNOLD_ROOT={arnoldRoot} BUILD_DIR={cwd}/gafferDependenciesBuild ./build/buildAll.sh ".format(
+			cwd = os.getcwd(),
 			**formatVariables
 		)
-		sys.stderr.write( copyCommand + "\n" )
-		subprocess.check_call( copyCommand, shell = True )
 
-	sys.exit( 0 )
+	sys.stderr.write( buildCommand + "\n" )
+	subprocess.check_call( buildCommand, shell=True )
 
-if os.path.exists( "/.dockerenv" ) and args.project == "gaffer" :
+	# Upload the build
 
-	# Start an X server so we can generate screenshots when the
-	# documentation builds.
-	os.system( "Xvfb :99 -screen 0 1280x1024x24 &" )
-	os.environ["DISPLAY"] = ":99"
-	os.system( "metacity&" )
+	if args.upload :
 
-# Download source code
-
-sourceURL = "https://github.com/{organisation}/{project}/archive/{version}.tar.gz".format( **formatVariables )
-sys.stderr.write( "Downloading source \"%s\"\n" % sourceURL )
-
-sourceDirName = "{project}-{version}-source".format( **formatVariables )
-tarFileName = "{0}.tar.gz".format( sourceDirName )
-downloadCommand = "curl -L {0} > {1}".format( sourceURL, tarFileName )
-sys.stderr.write( downloadCommand + "\n" )
-subprocess.check_call( downloadCommand, shell = True )
-
-sys.stderr.write( "Decompressing source to \"%s\"\n" % sourceDirName )
-
-shutil.rmtree( sourceDirName, ignore_errors = True )
-os.makedirs( sourceDirName )
-subprocess.check_call( "tar xf %s -C %s --strip-components=1" % ( tarFileName, sourceDirName ), shell = True )
-os.chdir( sourceDirName )
-
-# Download precompiled dependencies. We do this using the
-# same script that is used to download the dependencies for
-# testing on Travis, so that release builds are always made
-# against the same dependencies we have tested against.
-
-if args.project == "gaffer" :
-	subprocess.check_call( "./config/travis/installDependencies.sh", shell = True )
-
-# Perform the build.
-
-if args.project == "gaffer" :
-
-	# We run SCons indirectly via `python` so that it uses our
-	# preferred python from the environment. SCons itself
-	# unfortunately hardcodes `/usr/bin/python`, which might not
-	# have the modules we need to build the docs.
-	buildCommand = "python `which scons` package PACKAGE_FILE={uploadFile} ENV_VARS_TO_IMPORT=PATH DELIGHT_ROOT={delight} ARNOLD_ROOT={arnoldRoot} OPTIONS='' -j {cpus}".format(
-		cpus=multiprocessing.cpu_count(), **formatVariables
-	)
-
-else :
-
-	buildCommand = "env RMAN_ROOT={delight} ARNOLD_ROOT={arnoldRoot} BUILD_DIR={cwd}/gafferDependenciesBuild ./build/buildAll.sh ".format(
-		cwd = os.getcwd(),
-		**formatVariables
-	)
-
-sys.stderr.write( buildCommand + "\n" )
-subprocess.check_call( buildCommand, shell=True )
-
-# Upload the build
-
-if args.upload :
-
-	uploadCommand = (
-		'curl {auth}'
-		' -H "Content-Type: application/zip"'
-		' --data-binary @{uploadFile} "{uploadURL}"'
-		' -o /tmp/curlResult.txt' # Must specify output file in order to get progress output
-	).format(
-		uploadURL = "https://uploads.github.com/repos/{organisation}/{project}/releases/{id}/assets?name={uploadName}".format(
-			id = releaseId(),
-			uploadName = os.path.basename( formatVariables["uploadFile"] ),
+		uploadCommand = (
+			'curl {auth}'
+			' -H "Content-Type: application/zip"'
+			' --data-binary @{uploadFile} "{uploadURL}"'
+			' -o /tmp/curlResult.txt' # Must specify output file in order to get progress output
+		).format(
+			uploadURL = "https://uploads.github.com/repos/{organisation}/{project}/releases/{id}/assets?name={uploadName}".format(
+				id = releaseId(),
+				uploadName = os.path.basename( formatVariables["uploadFile"] ),
+				**formatVariables
+			),
 			**formatVariables
-		),
-		**formatVariables
-	)
+		)
 
-	sys.stderr.write( "Uploading package\n" )
-	sys.stderr.write( uploadCommand + "\n" )
+		sys.stderr.write( "Uploading package\n" )
+		sys.stderr.write( uploadCommand + "\n" )
 
-	subprocess.check_call( uploadCommand, shell = True )
+		subprocess.check_call( uploadCommand, shell = True )
